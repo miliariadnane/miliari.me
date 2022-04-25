9df964dc7436bc59523131b090924a8fb869deb4
miliariadnane <miliari.adnane@gmail.com>
2022-04-25 02:04:25 +0000

    typo

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -17,7 +17,7 @@ const LayoutWrapper = ({ children }) => {
               <div className="flex items-center justify-between">
                 {/* <div className="mr-3">
                   <Logo />
-                </div> */}
+                </div> **/}
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
